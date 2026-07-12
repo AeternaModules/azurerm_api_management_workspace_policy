7d4c6d72fa0897393a0ba29812cb437ff90478ad
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:00:22 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "api_management_workspace_policies_id" {
+  description = "Map of id values across all api_management_workspace_policies, keyed the same as var.api_management_workspace_policies"
+  value       = { for k, v in azurerm_api_management_workspace_policy.api_management_workspace_policies : k => v.id }
+}
 output "api_management_workspace_policies_api_management_workspace_id" {
   description = "Map of api_management_workspace_id values across all api_management_workspace_policies, keyed the same as var.api_management_workspace_policies"
   value       = { for k, v in azurerm_api_management_workspace_policy.api_management_workspace_policies : k => v.api_management_workspace_id }
