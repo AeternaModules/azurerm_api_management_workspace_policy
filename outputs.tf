0950c247cbc22c2c6ba4d62fb9f1bee50b5f9b98
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:40 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "api_management_workspace_policies" {
-  description = "All api_management_workspace_policy resources"
-  value       = azurerm_api_management_workspace_policy.api_management_workspace_policies
-}
 output "api_management_workspace_policies_api_management_workspace_id" {
-  description = "List of api_management_workspace_id values across all api_management_workspace_policies"
-  value       = [for k, v in azurerm_api_management_workspace_policy.api_management_workspace_policies : v.api_management_workspace_id]
+  description = "Map of api_management_workspace_id values across all api_management_workspace_policies, keyed the same as var.api_management_workspace_policies"
+  value       = { for k, v in azurerm_api_management_workspace_policy.api_management_workspace_policies : k => v.api_management_workspace_id }
 }
 output "api_management_workspace_policies_xml_content" {
-  description = "List of xml_content values across all api_management_workspace_policies"
-  value       = [for k, v in azurerm_api_management_workspace_policy.api_management_workspace_policies : v.xml_content]
+  description = "Map of xml_content values across all api_management_workspace_policies, keyed the same as var.api_management_workspace_policies"
+  value       = { for k, v in azurerm_api_management_workspace_policy.api_management_workspace_policies : k => v.xml_content }
 }
 output "api_management_workspace_policies_xml_link" {
-  description = "List of xml_link values across all api_management_workspace_policies"
-  value       = [for k, v in azurerm_api_management_workspace_policy.api_management_workspace_policies : v.xml_link]
+  description = "Map of xml_link values across all api_management_workspace_policies, keyed the same as var.api_management_workspace_policies"
+  value       = { for k, v in azurerm_api_management_workspace_policy.api_management_workspace_policies : k => v.xml_link }
 }
 
