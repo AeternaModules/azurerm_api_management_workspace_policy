057b5d8db0f9bb48e0630ea40626045e827383ad
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:46:13 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "api_management_workspace_policies_id" {
   description = "Map of id values across all api_management_workspace_policies, keyed the same as var.api_management_workspace_policies"
-  value       = { for k, v in azurerm_api_management_workspace_policy.api_management_workspace_policies : k => v.id }
+  value       = { for k, v in azurerm_api_management_workspace_policy.api_management_workspace_policies : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "api_management_workspace_policies_api_management_workspace_id" {
   description = "Map of api_management_workspace_id values across all api_management_workspace_policies, keyed the same as var.api_management_workspace_policies"
-  value       = { for k, v in azurerm_api_management_workspace_policy.api_management_workspace_policies : k => v.api_management_workspace_id }
+  value       = { for k, v in azurerm_api_management_workspace_policy.api_management_workspace_policies : k => v.api_management_workspace_id if v.api_management_workspace_id != null && length(v.api_management_workspace_id) > 0 }
 }
 output "api_management_workspace_policies_xml_content" {
   description = "Map of xml_content values across all api_management_workspace_policies, keyed the same as var.api_management_workspace_policies"
-  value       = { for k, v in azurerm_api_management_workspace_policy.api_management_workspace_policies : k => v.xml_content }
+  value       = { for k, v in azurerm_api_management_workspace_policy.api_management_workspace_policies : k => v.xml_content if v.xml_content != null && length(v.xml_content) > 0 }
 }
 output "api_management_workspace_policies_xml_link" {
   description = "Map of xml_link values across all api_management_workspace_policies, keyed the same as var.api_management_workspace_policies"
-  value       = { for k, v in azurerm_api_management_workspace_policy.api_management_workspace_policies : k => v.xml_link }
+  value       = { for k, v in azurerm_api_management_workspace_policy.api_management_workspace_policies : k => v.xml_link if v.xml_link != null && length(v.xml_link) > 0 }
 }
 
